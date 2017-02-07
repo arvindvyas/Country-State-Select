df934e2b345cc962c77aeb50184c4874f975cb61
Arvind Vyas <arvind.vyas@supernova.xyz>
2017-02-07 21:02:49 +0530

    Add rails5 support

--- lib/country_state_select.rb
+++ lib/country_state_select.rb
@@ -64,6 +64,8 @@ module CountryStateSelect
 end
 
 case ::Rails.version.to_s
+  when /^5/
+    require 'country_state_select/engine'
   when /^4/
     require 'country_state_select/engine'
   when /^3\.[12]/

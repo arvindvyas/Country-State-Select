8ec1a5732ce4f6c9449db529f2cd741b590dbd6e
sreena k <sreenanair.k@gmail.com>
2018-12-27 11:17:01 +0530

    Update cscs_controller.rb

--- app/controllers/country_state_select/cscs_controller.rb
+++ app/controllers/country_state_select/cscs_controller.rb
@@ -2,19 +2,19 @@
 module CountryStateSelect
   class CscsController < ApplicationController
     def find_states
-      csc =   CS.states(params[:country_id])
+      @csc =   CS.states(params[:country_id])
 
        respond_to do |format|
-         format.json { render :json => csc.to_a}
+         format.json { render :json =>{:states=> @csc.to_a}}
       end
     end
 
     # Sent it to state_id and  country id it will return cities of that states
     def find_cities
-      cities = CS.cities(params[:state_id].to_sym, params[:country_id].to_sym)
+      @cities = CS.cities(params[:state_id].to_sym, params[:country_id].to_sym)
 
       respond_to do |format|
-         format.json { render :json => cities.to_a}
+         format.json { render :json => {:cities=>@cities.to_a}}
       end
     end
   end
